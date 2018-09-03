916dacc31c9e0e1a3034714ba566a6fc6ec99fbd
Helice <helice@live.fr>
2018-09-03 15:34:25 +0200

    Modif exo 2

--- exercise_2/exercise_nidhal.js
+++ exercise_2/exercise_nidhal.js
@@ -1,7 +1,8 @@
-function insertRow() {
-	var foo = document.getElementById('myTable').insertRow(0);
-	var cell1 = foo.insertCell(0);
-	var cell2 = foo.insertCell(1);
+function myFunction() {
+	var table = document.getElementById('myTable');
+	var row = table.insertRow(0)
+	var cell1 = row.insertCell(0);
+	var cell2 = row.insertCell(1);
 	cell1.innerHTML = "NvllLigne Cellule1";
 	cell2.innerHTML = "NvllLigne Cellule2";
 }
