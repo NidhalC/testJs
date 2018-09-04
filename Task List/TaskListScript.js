b54a9e65bc24d07ae4415079fc4840d88d031a6e
Helice <helice@live.fr>
2018-09-04 09:57:19 +0200

    Task List v3.1

--- Task List/TaskListScript.js
+++ Task List/TaskListScript.js
@@ -19,14 +19,6 @@ for (i = 0; i < close.length; i++) {
   }
 }
 
-// Add a "checked" symbol when clicking on a list item
-var list = document.querySelector('ul');
-list.addEventListener('click', function(ev) {
-  if (ev.target.tagName === 'LI') {
-    ev.target.classList.toggle('checked');
-  }
-}, false);
-
 // Create a new list item when clicking on the "Add" button
 function newElement() {
   var li = document.createElement("li");
@@ -54,6 +46,7 @@ function newElement() {
   }
 } 
 
+//Delete all tasks
 function erase() {
     var x = document.getElementsByTagName("li");
     for(var i = x.length - 1; 0 <= i; i--){
