f205f58d2f8232a44e0e30c6f5760c8683910b98
Helice <helice@live.fr>
2018-09-04 15:12:06 +0200

    Modif innerHTML en innerText

--- Task List/TaskListScript.js
+++ Task List/TaskListScript.js
@@ -79,7 +79,7 @@ function filtrer() {
     // Loop through all list items, and hide those who don't match the search query
     for (i = 0; i < li.length; i++) {
         p = li[i];
-        if (p.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        if (p.innerText.toUpperCase().indexOf(filter) > -1) {
             li[i].style.display = "";
         } else {
             li[i].style.display = "none";
